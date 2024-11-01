f8604849dc7f8d1af8a1f3ee03fcbf42c61e52e3
lukethavyxay <lthavyxay@gmail.com>
2024-10-31 19:36:54 -0700

    lab 8

--- lab1/js/lab.js
+++ lab1/js/lab.js
@@ -22,3 +22,45 @@ main();
 
 var apples=5;
 console.log(apples);
+
+// calculator from section 0
+  //rectangle
+  // numbers 0-9 input arrays, prompt,
+
+function userInput() {
+
+let number;
+number=window.prompt("Please provide a number between 0 and 9");
+console.log(number);
+return number;
+
+
+
+  }
+
+  let numberFirst = userInput();
+  let numberSecond = userInput();
+
+  console.log("numberFirst is " + numberFirst)
+  console.log("numberSecond is " + numberSecond)
+
+ 
+results = numberFirst + numberSecond
+
+$(".calculator").append(numberFirst + " * ");
+$(".calculator").append(numberSecond + " * ");
+$(".calculator").append(results);
+
+
+
+
+
+console.log(results);
+
+
+
+
+
+  // addition operation
+  // output
+  
---
+++ lab8/js/lab8.js
@@ -0,0 +1,33 @@
+/**
+ * Author:    Luke Thavyxay
+ * Created:   31 October 2024
+ * 
+ * My website
+ **/
+
+//Create an array of numbers and assign it to a variable.
+var array = [1, 2, 3, 4, 5];
+//Create a named function that will take a single parameter, do a calculation on it (sqrt, sqr, addition, etc), and return the result.
+function addTwo(array){
+  return array + 2;
+}
+
+//This whole comments of code are not working
+  //call function 
+  //document.writeln(addTwo(5));
+  //console.log(array.map(addTwo));
+  //document.writeln(array.map(addtwo));
+
+let res2=array.map(function(array){
+  var results = array*2
+  return results;
+})
+
+var mapResults = array.map(addTwo);
+console.log("My array: ", array)
+console.log("Results adding 2", mapResults)
+console.log('Results multiplying by 2',res2)
+
+//attempted to do lab X but failed
+  //var mapResults = "Your map results here",mapResults;
+  //$("#output").html(mapResults); 
